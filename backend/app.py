143e97936d273ed4a29a5b110fb7a4a1787bc87a
satyajit <satyajiit0@gmail.com>
2024-11-30 20:45:14 +0530

    cors handled

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from backend.endpoints import register_routes
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
+
+    # Allow CORS for the frontend
+    CORS(app, resources={r"/*": {"origins": r"http://localhost:\d+"}})
+
     register_routes(app)
     return app
